a8021556feb3dbe0cf6e3a4815c46fbfe5417e5d
LongkerDandy <longkerdandy@gmail.com>
2014-12-15 19:34:02 +0800

    const methods

--- evo-arangodb/src/main/java/com/github/longkerdandy/evo/arangodb/Const.java
+++ evo-arangodb/src/main/java/com/github/longkerdandy/evo/arangodb/Const.java
@@ -10,4 +10,15 @@ public class Const {
     public static final String COLLECTION_USERS = "users";
     public static final String COLLECTION_DEVICES = "devices";
     public static final String EDGE_USER_DEVICE = "user_device";
+
+    /**
+     * Generate UserDeviceRelation Id from User Id and Device Id
+     *
+     * @param uid User Id
+     * @param did Device Id
+     * @return UserDeviceRelation Id
+     */
+    public static String relationId(String uid, String did) {
+        return uid + "_" + did;
+    }
 }
