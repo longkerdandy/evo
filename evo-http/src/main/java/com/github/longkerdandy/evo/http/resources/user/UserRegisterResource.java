1c3a9a1f3da7193ea92c620682f6b0f26023200e
LongkerDandy <longkerdandy@gmail.com>
2015-04-22 16:41:10 +0800

    add logger

--- evo-http/src/main/java/com/github/longkerdandy/evo/http/resources/user/UserRegisterResource.java
+++ evo-http/src/main/java/com/github/longkerdandy/evo/http/resources/user/UserRegisterResource.java
@@ -12,6 +12,8 @@ import com.github.longkerdandy.evo.http.resources.AbstractResource;
 import com.github.longkerdandy.evo.http.storage.AerospikeStorageManager;
 import com.google.common.base.Optional;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
@@ -23,6 +25,9 @@ import javax.ws.rs.core.MediaType;
 @Path("/users/register")
 @Produces(MediaType.APPLICATION_JSON)
 public class UserRegisterResource extends AbstractResource {
+
+    // Logger
+    private static final Logger logger = LoggerFactory.getLogger(UserRegisterResource.class);
 
     protected static final int MOBILE_VERIFY_CODE_LENGTH = 6;
     protected static final int MOBILE_VERIFY_CODE_TTL = 180;    // seconds
@@ -36,6 +41,7 @@ public class UserRegisterResource extends AbstractResource {
     public ResultEntity<Boolean> exist(@HeaderParam("Accept-Language") @DefaultValue("zh") String lang,
                                        @QueryParam("mobile") Optional<String> mobile,
                                        @QueryParam("email") Optional<String> email) {
+        logger.trace("Process exist request with params: mobile {} email {}", mobile.get(), email.get());
         // validate
         if (!mobile.isPresent() && !email.isPresent()) {
             throw new ValidateException(new ErrorEntity(ErrorCode.MISSING_FIELD, lang));
@@ -71,6 +77,7 @@ public class UserRegisterResource extends AbstractResource {
     public ResultEntity<String> verify(@HeaderParam("Accept-Language") @DefaultValue("zh") String lang,
                                        @QueryParam("type") @DefaultValue("mobile") String type,
                                        @Valid UserRegisterEntity r) {
+        logger.trace("Process exist request with params: type {}", type);
         // validate
         if (r == null || r.getUser() == null) {
             throw new ValidateException(new ErrorEntity(ErrorCode.MISSING_FIELD, lang));
@@ -100,6 +107,7 @@ public class UserRegisterResource extends AbstractResource {
             // create mobile verify code
             String code = RandomStringUtils.randomNumeric(MOBILE_VERIFY_CODE_LENGTH);
             storage().updateVerify(r.getUser().getMobile(), code, MOBILE_VERIFY_CODE_TTL);
+            logger.debug("Created a verify code for mobile {}", r.getUser().getMobile());
             // send to mq
             producer().sendSmsMessage(new SmsMessage<>(r.getUser().getMobile(), SmsMessage.TYPE_VERIFY_CODE, new SmsVerifyCode(code)));
 
