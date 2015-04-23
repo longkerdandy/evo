94797758f651aa84df2c3b3b4523b5673d746538
LongkerDandy <longkerdandy@gmail.com>
2015-04-23 14:06:59 +0800

    add gradle java application plugin; add config file

--- evo-sms/src/main/java/com/github/longkerdandy/evo/service/sms/SmsApplication.java
+++ evo-sms/src/main/java/com/github/longkerdandy/evo/service/sms/SmsApplication.java
@@ -1,22 +1,22 @@
 package com.github.longkerdandy.evo.service.sms;
 
 import com.github.longkerdandy.evo.service.sms.mq.SmsLegacyConsumer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.configuration.PropertiesConfiguration;
 
 import java.util.Properties;
 
 /**
  * SMS Service
  */
-public class SmsService {
-
-    private static final String ZK_HOST = "192.168.253.68:2181";
+public class SmsApplication {
 
     public static void main(String args[]) throws Exception {
+        // load config
+        PropertiesConfiguration config = new PropertiesConfiguration("config/sms.properties");
+
         // start mq consumer
         Properties props = new Properties();
-        props.put("zookeeper.connect", ZK_HOST);
+        props.put("zookeeper.connect", config.getString("mq.zk.hosts"));
         props.put("group.id", "sms");
         SmsLegacyConsumer consumer = new SmsLegacyConsumer(props, 1);
     }
