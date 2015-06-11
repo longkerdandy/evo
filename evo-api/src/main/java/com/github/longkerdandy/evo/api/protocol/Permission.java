0b2876146e9ab16cb106000e8a0deabd3fb17d7a
LongkerDandy <longkerdandy@gmail.com>
2015-06-11 16:21:54 +0800

    comment unused permission atm

--- evo-api/src/main/java/com/github/longkerdandy/evo/api/protocol/Permission.java
+++ evo-api/src/main/java/com/github/longkerdandy/evo/api/protocol/Permission.java
@@ -9,7 +9,7 @@ public class Permission {
     public static final int NONE = 0;       // Level 0  Default, No Privilege
     public static final int READ = 3;       // Level 3  Read Privilege
     public static final int READ_WRITE = 6; // Level 6  Read & Write Privilege
-    public static final int FULL = 9;       // Level 9  Full Privilege
+    // public static final int FULL = 9;       // Level 9  Full Privilege
     public static final int OWNER = 10;     // Level 10 Owner Privilege
 
     private Permission() {
